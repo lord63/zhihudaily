7638571c2b7e52f22c8a137fdea69809d8bb2d13
lord63 <lord63.j@gmail.com>
2015-02-09 21:22:46 +0800

    Initial commit.
    can work now. we have the lastest news as a list.

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    session = requests.Session()
+    session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
+                            x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
+    request = session.get('http://news.at.zhihu.com/api/1.2/news/latest')
+    date = request.json()['display_date']
+    news_list = [
+        [item['title'], item['url']] for item in request.json()['news']]
+    story_list = [
+        [item['title'], item['url']] for item in request.json()['top_stories']]
+    lists = news_list + story_list
+    return render_template("index.html", lists=lists, date=date)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
