c727ff205002220b2b05df31e2bfa3edd9944954
lord63 <lord63.j@gmail.com>
2015-02-10 20:30:29 +0800

    Better url route: hostname/before/<date>
    rather than hostname/?date=<date>
    though the function index and before are similar. I think the
    original one seems puzzled, because hosname/?date=20150210 shows the
    news of the 20150209.

--- app.py
+++ app.py
@@ -9,26 +9,32 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+@app.route('/before/<date>')
+def before(date):
+    session = requests.Session()
+    session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
+                            x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
+    r = session.get(
+        'http://news.at.zhihu.com/api/1.2/news/before/{0}'.format(date))
+    display_date = r.json()['display_date']
+    date = r.json()["date"]
+    news_list = [
+        [item['title'], item['share_url']] for item in r.json()['news']]
+    return render_template("index.html", lists=news_list,
+                           display_date=display_date, date=date)
+
+
 @app.route('/')
 def index():
     session = requests.Session()
     session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
                             x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
     date = request.args.get('date')
-    if date:
-        r = session.get(
-            'http://news.at.zhihu.com/api/1.2/news/before/{0}'.format(date))
-        # story_list = []
-    else:
-        r = session.get('http://news.at.zhihu.com/api/1.2/news/latest')
-        print 'yes'
-        # story_list = [
-            # [item['title'], item['share_url']] for item in r.json()['top_stories']]
+    r = session.get('http://news.at.zhihu.com/api/1.2/news/latest')
     display_date = r.json()['display_date']
     date = r.json()["date"]
     news_list = [
         [item['title'], item['share_url']] for item in r.json()['news']]
-    # lists = story_list + news_list
     return render_template("index.html", lists=news_list,
                            display_date=display_date, date=date)
 
