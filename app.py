269524f48f5bc32ef85b5993f9f9299aaa7bd2c1
lord63 <lord63.j@gmail.com>
2015-02-10 09:59:43 +0800

    Use share_url instead of url

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def index():
     request = session.get('http://news.at.zhihu.com/api/1.2/news/latest')
     date = request.json()['display_date']
     news_list = [
-        [item['title'], item['url']] for item in request.json()['news']]
+        [item['title'], item['share_url']] for item in request.json()['news']]
     story_list = [
-        [item['title'], item['url']] for item in request.json()['top_stories']]
-    lists = news_list + story_list
+        [item['title'], item['share_url']] for item in request.json()['top_stories']]
+    lists = story_list + news_list
     return render_template("index.html", lists=lists, date=date)
 
 
