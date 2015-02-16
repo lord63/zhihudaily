e89e90d897eb0a11fe65145dca959dfb63e25715
lord63 <lord63.j@gmail.com>
2015-02-16 23:09:48 +0800

    Try pagination and pages, #3
    - page 能用了，但界面有点小问题，<h3>日期</h3> 没有换行显示
    - 使用 peewee
    - 将 test.html 重命名为 with_image.html

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import datetime
+from math import ceil
+import json
 
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
+from peewee import *
+
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+SECRET_KEY = 'hin6bab8ge25*r=x&amp;+5$0kn=-#log$pt^#@vrqjld!^2ci@g*b'
+
+database = SqliteDatabase('zhihudaily.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Zhihudaily(BaseModel):
+    date = IntegerField()
+    json_news = CharField()
+
+
+def create_tables():
+    database.connect()
+    database.create_tables([Zhihudaily])
+
+
+@app.before_request
+def before_request():
+    g.db = database
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 
 @app.route('/before/<date>')
@@ -29,7 +63,6 @@ def index():
     session = requests.Session()
     session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
                             x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
-    date = request.args.get('date')
     r = session.get('http://news.at.zhihu.com/api/1.2/news/latest')
     display_date = r.json()['display_date']
     date = r.json()["date"]
@@ -39,8 +72,8 @@ def index():
                            display_date=display_date, date=date)
 
 
-@app.route('/test')
-def test():
+@app.route('/withimage')
+def with_image():
     session = requests.Session()
     session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
                             x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
@@ -48,8 +81,37 @@ def test():
     display_date = r.json()['display_date']
     date = r.json()["date"]
     news_list = [item for item in r.json()['news']]
-    return  render_template('test.html', lists=news_list,
-                            display_date=display_date,date=date)
+    request.environ['Referer'] = 'http://daily.zhihu.com/'
+    return render_template('with_image.html', lists=news_list,
+                           display_date=display_date, date=date)
+
+
+@app.route('/pages')
+@app.route('/pages/<int:page>')
+def pages(page=1):
+    session = requests.Session()
+    session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux \
+                            x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'})
+    r = session.get('http://news.at.zhihu.com/api/1.2/news/latest')
+    display_date = r.json()['display_date']
+    date = r.json()["date"]
+    news_list = [item for item in r.json()['news']]
+    request.environ['Referer'] = 'http://daily.zhihu.com/'
+    news = Zhihudaily.select().paginate(page, 7)
+    records = []
+    for i in news:
+        temp = json.loads(i.json_news)
+        records.append({"date": i.date, "news": temp})
+    return render_template('pages.html', lists=news_list,
+                           display_date=display_date, date=date,
+                           page=page, records=records)
+
+
+@app.route('/pagination')
+def pagination():
+    pages = int(ceil(Zhihudaily.select().count() / 7))
+    return render_template('pagination.html', pages=pages)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
