40f5ac463719903f61bbd2f1a3966a5958ce2b6c
lord63 <lord63.j@gmail.com>
2015-10-28 10:26:43 +0800

    Remove the unused date variable in pages UI

--- zhihudaily/views/pages.py
+++ zhihudaily/views/pages.py
@@ -23,19 +23,18 @@ def pages(page=1):
     """The page the 分页 UI."""
     # TODO: don't send useless requests.
     r = make_request('http://news.at.zhihu.com/api/1.2/news/latest')
-    (display_date, date, news_list) = get_news_info(r)
+    (display_date, _, news_list) = get_news_info(r)
     news_list = handle_image(news_list)
     news = Zhihudaily.select().order_by(
         Zhihudaily.date.desc()).paginate(page, 4)
     records = []
     for i in news:
         news = handle_image(json.loads(i.json_news))
-        records.append({"date": i.date, "news": news,
-                        "display_date": i.display_date})
+        records.append({"news": news, "display_date": i.display_date})
     pagination = Pagination(page=page, total=Zhihudaily.select().count(),
                             per_page=4, inner_window=7, outer_window=3,
                             css_framework='bootstrap3')
     return render_template('pages.html', lists=news_list,
-                           display_date=display_date, date=date,
+                           display_date=display_date,
                            page=page, records=records,
                            pagination=pagination)
