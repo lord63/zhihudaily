18386fe42fd7f2b83e34c9946bd020268827e0e8
lord63 <lord63.j@gmail.com>
2015-11-11 21:03:31 +0800

    Simplify date utils in /before route

--- zhihudaily/views/index.py
+++ zhihudaily/views/index.py
@@ -3,11 +3,9 @@
 
 from __future__ import absolute_import, unicode_literals
 
-import datetime
-
 from flask import render_template, Blueprint, request, redirect, url_for, json
 
-from zhihudaily.utils import make_request, Date
+from zhihudaily.utils import Date
 from zhihudaily.cache import cache
 from zhihudaily.models import Zhihudaily
 
@@ -39,29 +37,22 @@ def full_request_path():
 def before(date):
     """For 文字 UI and 图片 UI, before today."""
 
-    today = datetime.date.today().strftime('%Y%m%d')
-    if int(today) <= int(date):
+    day = Date()
+    if int(day.today) <= int(date):
         if request.args.get('image', 'False') == 'True':
             return redirect(url_for('image_ui.with_image'))
         else:
             return redirect(url_for('text_ui.index'))
 
     news = Zhihudaily.select().where(Zhihudaily.date == int(date)).get()
-    display_date, news_list = news.display_date, json.loads(news.json_news)
-
-    day_before = (
-        datetime.datetime.strptime(date, '%Y%m%d') - datetime.timedelta(1)
-    ).strftime('%Y%m%d')
-    day_after = (
-        datetime.datetime.strptime(date, '%Y%m%d') + datetime.timedelta(1)
-    ).strftime('%Y%m%d')
 
     template_name = {
         'False': 'index.html', 'True': 'with_image.html'
     }.get(request.args['image'], 'False')
 
-    return render_template(template_name, lists=news_list,
-                           display_date=display_date,
-                           day_before=day_before,
-                           day_after=day_after,
+    return render_template(template_name,
+                           lists=json.loads(news.json_news),
+                           display_date=news.display_date,
+                           day_before=day.day_before,
+                           day_after=day.day_after,
                            is_today=False)
