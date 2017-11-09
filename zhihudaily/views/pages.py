84919815e0d17cd5509d33927623b93ad458bfcf
lord63 <lord63.j@gmail.com>
2017-11-09 16:18:47 +0800

    Remove unnecessary unicode_literals

--- zhihudaily/views/pages.py
+++ zhihudaily/views/pages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
 from flask import render_template, Blueprint, json
 from flask.ext.paginate import Pagination
